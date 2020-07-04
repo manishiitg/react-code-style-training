cc431e96845fedf1de60ff639e0d2f804627c947
manish <manish@laptoplenovo>
2020-07-04 11:04:27 +0530

    ui components css v2

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,29 +1,23 @@
 import React from "react"
 import styled from "styled-components"
 
-const ButtonStyle = styled.button`
-   font-size: 1rem;
+const ButtonStyle = styled.button.attrs(props => ({
+    size: props.size || "1em"
+}))`
+   font-size: ${props => props.size};
    text-align: center;
 `;
 
 const Button = (props) => {
     return (
-        <ButtonStyle onClick={props.onClick}>
-            {props.text}
-        </ButtonStyle>
-    )
-}
-
-const LargeButtonStyle = styled(ButtonStyle)`
-   font-size: 2rem
-`;
-
-export const LargeButton = (props) => {
-    return (
-        <LargeButtonStyle onClick={props.onClick}>
-            {props.text}
-        </LargeButtonStyle>
+        <ButtonStyle {...props} onClick={props.onClick} value={props.chidren} />
     )
 }
 
 export default Button
+
+export const LargeButton = (props) => {
+    return (
+        <Button {...props} size="2em"></Button>
+    )
+}
